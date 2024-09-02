c257840f920782eafb05a949150f7984d4a2e2ed
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-09-02 14:13:25 +0300

    tabs styling

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ const index = () => {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style='auto'/>
-      <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
+      <Link href="/home" style={{color:'blue'}}>Go to home</Link>
     </View>
   )
 }
